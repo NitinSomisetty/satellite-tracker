e1115a427eea08d0d0160e75f2d540cebff06300
Nitin Somisetty <nitin.somisetty@gmail.com>
2026-01-18 15:41:13 +0530

    initial

---
+++ main.py
@@ -0,0 +1,15 @@
+from skyfield import *
+import requests
+
+url = "https://celestrak.org/NORAD/elements/stations.txt"
+data = requests.get(url).text.splitlines()
+
+name = data[0]
+line1 = data[1]
+line2 = data[2]
+
+from skyfield.api import load
+ts=load.tmescale()
+sat = EarthSatellite(line1, line2, name, ts)
+
+t = ts.now()
